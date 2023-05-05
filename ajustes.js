20e10454099b73719bae5c41ce4a43e09f33bea8
ignaciosallent <ignacio.sallent.7e6@itb.cat>
2023-05-05 09:56:32 +0200

    nene

---
+++ ajustes.js
@@ -0,0 +1,18 @@
+actualizaReloj()
+function actualizaReloj(){
+    momentoActual = new Date()
+    hora = momentoActual.getHours()
+    minuto = momentoActual.getMinutes()
+    segundo = momentoActual.getSeconds()
+
+    if (hora < 10) hora = "0" + hora
+    if (minuto < 10) minuto = "0" + minuto
+    if (segundo < 10) segundo = "0" + segundo
+    
+    horaImprimible = hora + ":" + minuto + ":" + segundo
+    
+    document.title = horaImprimible
+    horaActual.innerHTML = horaImprimible
+    
+    setTimeout("actualizaReloj()", 1000)
+}
