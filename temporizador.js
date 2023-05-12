9a7446b07ed84f54102a37de8e031c90a08514f4
ignaciosallent <ignacio.sallent.7e6@itb.cat>
2023-05-12 09:51:22 +0200

    vcdfsw

--- temporizador.js
+++ temporizador.js
@@ -1,3 +1,4 @@
+//creo cuatro variables, para el audio, el boton de start y reset, y la seleccion
 var start = document.getElementById('start');
 var reset = document.getElementById('reset');
 var select = document.getElementById('sonidos-contador');
@@ -7,11 +8,10 @@ var h = document.getElementById("hour");
 var m = document.getElementById("minute");
 var s = document.getElementById("sec");
 
-//store a reference to the startTimer variable
+//guardamos una referencia a la variable startTimer
 var startTimer = null;
 
 start.addEventListener('click', function(){
-    //initialize the variable
     function startInterval(){
         startTimer = setInterval(function() {
             timer();
@@ -25,19 +25,19 @@ reset.addEventListener('click', function(){
     h.value = 0;
     m.value = 0;
     s.value = 0;
-    //stop the timer after pressing "reset"
+    //parar el contador tras pulsar el botón "reset
     stopInterval()
     console.log('Boton Reset, Variable Reiniciada', reset.value);
 })
 
 select.addEventListener('change', function(){
-    //get the selected audio file based on the selected option
+    //conseguimos el audio según la opción del select escogida
     var audioFile = 'musicas/' + select.value + '.mp3';
     //log para ver si se ejecuta el cambio de seleccion
     console.log('Option changed:', select.value);
-    //Verificacion de si la tura de los archivos de audio se esta generando correctamete
+    //Verificacion de si la ruta de los archivos de audio se esta generando correctamete
     console.log('Audio file:', audioFile);
-    //create a new Audio object with the selected file
+    //creamos un objeto Audio con el archivo escogido
     audio = new Audio(audioFile);
 })
 
@@ -51,11 +51,11 @@ function timer(){
         h.value = 0;
         m.value = 0;
         s.value = 0;
-        //play the selected audio file when the timer reaches 0
+        //reproducir el archivo de audio escogido cuando el temporizador llegue a 0
         if(audio){
             playButton.click();
         }
-        //stop the timer after reaching 0
+        //parar el timer cuando hayamos llegado a 0
         stopInterval();
     } else if(s.value != 0){
         s.value--;
@@ -75,8 +75,8 @@ playButton.addEventListener('click', function(){
     }
 });
 
-//stop the function after pressing the reset button, 
-//so the time wont go down when selecting a new time after pressing reset
+//parar la funcion despues de pulsar el boton reset
+// asi podemos poner un nuevo tiempo despues de pulsar reset
 function stopInterval() {
     clearInterval(startTimer);
 }
